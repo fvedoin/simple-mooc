f019ec3585a4d4e9bfc158271d3e67790306430c
Fernand <fernando.vgarcia@hotmail.com>
2020-04-13 02:47:51 -0300

    Cancelando inscrição nos cursos.

--- curso/courses/urls.py
+++ curso/courses/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import index, details, enrollment, announcements
+from .views import index, details, enrollment, announcements, undo_enrollment
 
 urlpatterns = [
     path('', index, name='index'),
     path('<slug:slug>/', details, name='details'),
     path('<slug:slug>/inscricao', enrollment, name='enrollment'),
+    path('<slug:slug>/cancelar-inscricao', undo_enrollment, name='undo_enrollment'),
     path('<slug:slug>/anuncios', announcements, name='announcements')
 ]
 
