b5e49630a5292ed208b6de9b0a8f7297d838928a
Fernand <fernando.vgarcia@hotmail.com>
2020-04-13 02:21:32 -0300

    Template de anúncios criado.

--- curso/courses/urls.py
+++ curso/courses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, details, enrollment
+from .views import index, details, enrollment, announcements
 
 urlpatterns = [
     path('', index, name='index'),
     path('<slug:slug>/', details, name='details'),
-    path('<slug:slug>/inscricao', enrollment, name='enrollment')
+    path('<slug:slug>/inscricao', enrollment, name='enrollment'),
+    path('<slug:slug>/anuncios', announcements, name='announcements')
 ]
 
