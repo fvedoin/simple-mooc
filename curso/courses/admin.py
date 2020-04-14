97cbab0fd0d4ba36eb1a980c700ded67112f6ce0
Fernand <fernando.vgarcia@hotmail.com>
2020-04-13 22:22:06 -0300

    Models de anúncios e comentários adicionadas.

--- curso/courses/admin.py
+++ curso/courses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, Enrollment, Announcement, Comment
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'start_date', 'created_at']
@@ -8,3 +8,4 @@ class CourseAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Course, CourseAdmin)
+admin.site.register([Enrollment, Announcement, Comment])
