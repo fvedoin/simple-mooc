3fe7f4139d3e2c758b82320e58b0ca340d610818
Fernand <fernando.vgarcia@hotmail.com>
2020-04-14 00:53:54 -0300

    Decorator para verificar permissão de acesso ao curso pelo aluno implementado.

--- curso/courses/views.py
+++ curso/courses/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Course, Enrollment, Announcement
 from .forms import ContactCourse, CommentForm
-
+from .decorators import enrollment_required
 
 def index(request):
     courses = Course.objects.all()
@@ -63,15 +63,9 @@ def undo_enrollment(request, slug):
 
 
 @login_required
+@enrollment_required
 def announcements(request, slug):
-    course = get_object_or_404(Course, slug=slug)
-    if not request.user.is_staff:
-        enrollment = get_object_or_404(
-            Enrollment, user=request.user, course=course
-        )
-        if not enrollment.is_approved():
-            messages.error(request, 'A sua inscrição está pendente.')
-            return redirect('accounts:dashboard')
+    course = request.course
     template = 'courses/announcements.html'
     context = {
         'course': course,
@@ -80,8 +74,9 @@ def announcements(request, slug):
     return render(request, template, context)
 
 @login_required
+@enrollment_required
 def show_announcement(request, slug, pk):
-    course = get_object_or_404(Course, slug=slug)
+    course = request.course
     announcement = get_object_or_404(course.announcements.all(), pk=pk)
     form = CommentForm(request.POST or None)
     if form.is_valid():
@@ -100,3 +95,4 @@ def show_announcement(request, slug, pk):
     }
 
     return render(request, template, context)
+
