cdc1319f6da8b47bcaba82e0fb23251ee59d2a9d
Fernand <fernando.vgarcia@hotmail.com>
2020-04-15 00:35:56 -0300

    Testes em diretório separado.

--- curso/courses/tests/test_views.py
+++ curso/courses/tests/test_views.py
@@ -4,7 +4,7 @@ from django.test.client import Client
 from django.urls import reverse
 from django.conf import settings
 
-from .models import Course
+from curso.courses.models import Course
 
 class ContactCourseTestCase(TestCase):
 
