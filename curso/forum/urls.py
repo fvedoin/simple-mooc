c8b5acbb47edb24f88b82524ba222a8b943b861c
Fernand <fernando.vgarcia@hotmail.com>
2020-04-17 03:44:45 -0300

    Proprietário do tópico pode definir resposta correta via AJAX.

--- curso/forum/urls.py
+++ curso/forum/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index, thread
+from .views import index, thread, reply_correct, reply_incorrect
 
 urlpatterns = [
     path('', index, name='index'),
     path('tag/<tag>', index, name='index_tagged'),
     path('<slug>', thread, name='thread'),
+    path('respostas/<int:pk>/correta', reply_correct, name='reply_correct'),
+    path('respostas/<int:pk>/incorreta', reply_incorrect, name='reply_incorrect')
 ]
--- curso/forum/views.py
+++ curso/forum/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, ListView, DetailView
+import json
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic import View, ListView, DetailView
+from django.http import HttpResponse
 from django.contrib import messages
 
-from .models import Thread
+from .models import Thread, Reply
 from .forms import ReplyForm
 
 # class ForumView(TemplateView):
@@ -69,5 +71,22 @@ class ThreadView(DetailView):
             context['form'] = ReplyForm()
         return self.render_to_response(context)
 
+class ReplyCorrectView(View):
+    correct = True
+
+    def get(self, request, pk):
+        reply = get_object_or_404(Reply, pk=pk, thread__author=request.user)
+        reply.correct = self.correct
+        reply.save()
+        message = 'Resposta atualizada com sucesso!'
+        if request.is_ajax():
+            data = {'success': True, 'message': message}
+            return HttpResponse(json.dumps(data), content_type='application/json')
+        else:
+            messages.success(request, message)
+            return redirect('forum:thread', slug=reply.thread.slug)
+
 index = ForumView.as_view()
 thread = ThreadView.as_view()
+reply_correct = ReplyCorrectView.as_view()
+reply_incorrect = ReplyCorrectView.as_view(correct=False)
